3875d6fb6bfe12e2aacc99ca25ce466c3f90e130
Дмитро Миронов <hattabith@gmail.com>
2025-06-10 22:10:11 +0300

    Create start mvp items 10062025

---
+++ Partial Discharge Analyzer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Partial_Discharge_Analyzer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
