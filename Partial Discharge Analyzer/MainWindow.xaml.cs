6eadbb03ccc358f25df7ca1c7683be7ce4b26428
Дмитро Миронов <hattabith@gmail.com>
2025-06-24 14:30:00 +0300

    Add Light\Dark Themes

--- Partial Discharge Analyzer/MainWindow.xaml.cs
+++ Partial Discharge Analyzer/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Partial_Discharge_Analyzer
 {
     /// <summary>
@@ -19,10 +20,39 @@ namespace Partial_Discharge_Analyzer
         public MainWindow()
         {
             InitializeComponent();
-            ConnComboBox.ItemsSource = new List<string> { "192.168.88.1", "192.168.88.1:3000", "192.168.88.1:5000", "192.168.2.127" };
 
+            // TODO Add RegExp to manage the connection string format
+            ConnComboBox.ItemsSource = new List<string> { "TCPIP0::192.168.2.127::5025::SOCKET", "192.168.88.1", "192.168.88.1:3000", "192.168.88.1:5000", "192.168.2.127" };
+            SCPIOutputTextBox.Text = "SCPI Output will be displayed here. \rExample:\r*IDN?\rMANUFACTURE INSTR0.01.1";
 
         }
-        
+
+        public void ApplyTheme(string themeName)
+        {
+            var dict = new ResourceDictionary();
+            dict.Source = new Uri($"Themes/{themeName}Theme.xaml", UriKind.Relative);
+
+            // Видаляємо попередню тему
+            var existing = Application.Current.Resources.MergedDictionaries
+                            .FirstOrDefault(d => d.Source != null && d.Source.OriginalString.Contains("Theme"));
+            if (existing != null)
+                Application.Current.Resources.MergedDictionaries.Remove(existing);
+
+            // Додаємо нову
+            Application.Current.Resources.MergedDictionaries.Add(dict);
+        }
+
+        private void ThemeSelector_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            if (((ComboBoxItem)((ComboBox)sender).SelectedItem).Content is string theme)
+            {
+                ApplyTheme(theme);
+            }
+        }
+
+        private void SCPIOutputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
